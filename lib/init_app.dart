5c3beaecef1fa33aa4752b19976b98e258b7111c
Nevalyurtsever <sinankanmaz20@gmail.com>
2024-06-09 16:04:18 +0300

    feat: init app configration

---
+++ lib/init_app.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+import 'core/services/fcm_service.dart';
+
+Future<void> initApp() async {
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  setPathUrlStrategy();
+  await Hive.initFlutter();
+  await Hive.openBox('settings');
+  await FcmService.requestNotificationPermission();
+  await FcmService.setForegroundNotification();
+  await FcmService.listenOnMessage();
+  await FcmService.onMessageOpenedApp();
+
+  if (!kDebugMode) {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  }
+}
